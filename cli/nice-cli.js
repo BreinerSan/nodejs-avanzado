68daef6c9503f0bdc50c9b52fa87a494e2986c3f
BreinerSan <breiner.sv@gmail.com>
2025-05-08 21:02:21 -0500

    Argumentos y flags en aplicacion CLI con minimist

---
+++ cli/nice-cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env-node
+
+const minimist = require('minimist');
+const args = minimist(process.argv.slice(2), {
+    string: ['name', 'role', 'company'],
+    boolean: ['saludo'],
+    default: {
+        saludo: false
+    },
+    alias: {
+        n: 'name',
+        r: 'role',
+        c: 'company',
+        s: 'saludo'
+    }
+});
+
+console.log(args);
+
+const signals = ['SIGINT', 'SIGTERM', 'SIGUSR1', 'SIGUSR2'];
+
+signals.forEach(signal => {
+    process.on(signal, () => {
+        console.log(`Recibido ${signal}`);
+        process.exit(0);
+    })
+})
