84ba2b78a59453c8ffe52d068888a3008827b658
BreinerSan <breiner.sv@gmail.com>
2025-05-10 21:22:39 -0500

    Creacion de un asistente de chat con ia como un cli

---
+++ cli/ia-assistant.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist';
+import { blue, green, red, yellow } from 'colorette';
+import inquirer from 'inquirer';
+import ora from 'ora';
+import { GoogleGenAI } from '@google/genai';
+
+const args = minimist(process.argv.slice(2), {
+    string: ['name'],
+    default: {
+        name: 'John Doe'
+    },
+    alias: {
+        n: 'name'
+    }
+});
+
+console.log(`${blue('Hello')} ${green(args.name)}!`);
+
+// Definimos el model y system prompot
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY || 'AIzaSyAdI6HW3-g8P7kzR-krOASh_AI_1R0U3bE';
+
+if (!GEMINI_API_KEY) {
+    console.error(red('Error: Gemini API key is not defined'));
+    process.exit(1);
+}
+
+const systemPrompt = 'Soy un un asistente experto en node.js y javascript y solo puedes responder preguntas sobre node.js, javascript, herramientas y mejores prácticas de Node.js. Si alguien me pregunta sobre temas fuera de ese alcance, como preguntas generales, responderé amablemente que no puedo ayudar con eso.';
+
+const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
+
+const conversation = [];
+
+async function generateResponse(prompt){
+    try {
+        const history = conversation.map((item) => {
+            return {
+                role: item.role,
+                parts: [{
+                    text: item.content
+                }]
+            }
+        });
+
+        const chat = ai.chats.create({
+            model: 'gemini-2.0-flash-001',
+            config: {
+                systemInstruction: systemPrompt,
+                temperature: 0.7,
+                maxOutputTokens: 1000
+            },
+            history
+        });
+
+        const result = await chat.sendMessage({
+            message: prompt
+        });
+        const response = result.text;
+        conversation.push({role: 'model', content: response});
+
+        return response;
+    } catch (error) {
+        console.log('Error al generar respuesta:', error);
+        return 'No se puede generar la respuesta';
+    }
+}
+
+// function principal
+async function main(){
+    try {
+        const { userInput } = await inquirer.prompt({
+            type: 'input',
+            name: 'userInput',
+            message: yellow(`${args.name}: `),
+            prefix: ''
+        });
+
+        conversation.push({role: 'user', content: userInput});
+
+        const spinner = ora({
+            text: 'Generando respuesta...',
+            color: 'cyan'
+        }).start();
+
+        const response = await generateResponse(userInput);
+
+        spinner.stop();
+
+        console.log(`${yellow('Profesor: ')} ${green(response)}`);
+
+        main();
+    } catch (error) {
+        if(error.name === 'ExitPromptError'){
+            console.log('Saliendo...');
+            process.exit(0);
+        }
+        console.log('Error al generar respuesta:', error);
+        process.exit(1);
+    }
+}
+
+main();
+
+process.on('SIGINT', () => {
+    console.log('Saliendo...');
+    process.exit(0);
+});
+
+process.on('uncaughtException', (err) => {
+    console.log('Error no capturado:', err);
+    process.exit(1);
+});
--- cli/nice-cli.js
+++ cli/nice-cli.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env-node
+#!/usr/bin/env node
 
 const minimist = require('minimist');
 const args = minimist(process.argv.slice(2), {
